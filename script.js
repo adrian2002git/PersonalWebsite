fd72b0f656be4b765744268d79d38bcb449202a3
AdrianOrlamuende <adrian.orlamuende@gmail.com>
2023-06-11 11:37:17 +0200

    First Commit

---
+++ script.js
@@ -0,0 +1,25 @@
+
+    $(document).ready(function() {
+    $(window).scroll(function() {
+        myFunction();
+    });
+
+    function myFunction() {
+    var navbar = $('header');
+    var sticky = navbar.offset().top;
+    var scrollPosition = $(window).scrollTop()+100;
+    console.log("Window:")
+    console.log($(window).scrollTop())
+        console.log("Sticky:")
+        console.log(sticky)
+    if ($(window).scrollTop() >= sticky) {
+    navbar.addClass('fixed-nav');
+
+} else {
+    navbar.removeClass('fixed-nav');
+}
+        if (scrollPosition <= 320) {
+            navbar.removeClass('fixed-nav');
+        }
+}
+});
